5fcf4560a60a1ec8fdf7170cedf23b9b5e688349
cristian <cpoliti@doxee.com>
2026-06-05 23:18:22 +0200

    Corso playwright: ultime commit

---
+++ tests/example2.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test("has title", async ({ page }) => {
+    await page.goto("https://www.playwright.dev/", { waitUntil: "commit" });
+    // await expect(page).toHaveTitle(/Playwright/);
+    expect(await page.title()).toBe("Fast and reliable end-to-end testing for modern web apps | Playwright");
+})
+test("get started link", async ({ page }) => {
+    await page.goto("https://www.playwright.dev/");
+    await page.getByRole("link", { name: "Get started" }).click();
+    await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible();
+})
---
+++ tests/myaccount.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('heading on My Account page', async ({ page }) => {
+    await page.goto("https://practicesoftwaretesting.com/account/");
+    await expect(page.getByRole("heading", { name: "My account" })).toBeVisible();
+})
