a0bd615b7e6439c9e4761824648f00577cb8f789
cristian <cpoliti@doxee.com>
2026-06-05 23:49:03 +0200

    aggiornamento progetto

--- test/myaccount.spec.ts
+++ test/myaccount.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test';
 
+test.use({ storageState: "auth.json" });
+
 test('heading on My Account page', async ({ page }) => {
     await page.goto("https://practicesoftwaretesting.com/account/");
     await expect(page.getByRole("heading", { name: "My account" })).toBeVisible();
